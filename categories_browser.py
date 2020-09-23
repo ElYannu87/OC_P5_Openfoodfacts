adce33447a29945337f4d16bc91e3c85cba9a625
ElYannu87 <y.driever@gmail.com>
2020-09-23 11:23:13 +0200

    Cleanup and added functions

--- categories_browser.py
+++ categories_browser.py
@@ -7,7 +7,6 @@ class CategoryBrowser:
 
     def __init__(self):
         self.conn = psycopg2.connect(dbname=DB, user=DB_USER, password=DB_PW, host=HOST, port=PORT)
-        self.categories_browser()
 
     def categories_browser(self):
         cursor = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -15,6 +14,7 @@ class CategoryBrowser:
                        "FROM product_category LEFT JOIN category ON product_category.category_id = category.id "
                        "GROUP BY category_id, category.name ORDER BY nb_products DESC LIMIT 30")
         query_result = cursor.fetchall()
+        cursor.close()
 
         page_size = 10
         page = 0
@@ -26,10 +26,16 @@ class CategoryBrowser:
         print("You have chosen : ", cat_options[category_choice][1])
         user_choice = (cat_options[category_choice][0])
 
+        return user_choice
+
+
+    def product_browser(self, category_id):
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cursor.execute("SELECT product.name as product_name, nutrition_grade, id FROM product "
                        "INNER JOIN product_category ON "
-                       "product.id = product_category.product_id WHERE category_id = %s", [user_choice])
+                       "product.id = product_category.product_id WHERE category_id = %s", [category_id])
         product_result = cursor.fetchall()
+        cursor.close()
         page_size = 10
         page = 0
         prod_options = product_result[(page * page_size):(page * page_size) + page_size]
@@ -37,5 +43,9 @@ class CategoryBrowser:
             print(index, value)
         product_choice = int(input("Please choose your product: "))
         print("You have chosen : ", prod_options[product_choice][0])
-        user_prod_choice = (prod_options[product_choice][2])
-        print(user_prod_choice)
+        product = prod_options[product_choice]
+
+        return {"id": product[2],"name": product[0], "nutrition_grade": product[1]}
+
+
+
--- substitues.py
+++ substitues.py
@@ -1,77 +1,30 @@
 import psycopg2
 from psycopg2 import extras
 from config import *
-import off_class as cl
-from categories_browser import *
-
-conn = psycopg2.connect(dbname=DB, user=DB_USER, password=DB_PW, host=HOST, port=PORT)
-
-new_data = 0
-products_list = []
-categories_list = []
 
 
-class Substitutes:
+class SubstitutesFinder:
 
-    def get_substitutes(self, product):
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        s_products = []
-        if product.nutrition_grade == "d" or product.nutrition_grade == "e":
-            cur.execute("""SELECT * FROM product WHERE category LIKE %s AND nutrition_grade='a' 
-                            OR category LIKE %s AND nutrition_grade='b'
-                            OR category LIKE %s AND nutrition_grade='b' """,
-                        (product.category, product.category, product.category))
-            result = cur.fetchall()
-            for element in result:
-                s_products.append(
-                    cl.Product(element['name'], element['store'], element['nutrition_grade'], element['url'],
-                               element['category']))
-            return s_products
+    def __init__(self):
+        self.conn = psycopg2.connect(dbname=DB, user=DB_USER, password=DB_PW, host=HOST, port=PORT)
 
-        else:
-            cur.execute("""SELECT * FROM product WHERE category LIKE %s AND nutrition_grade='a' """,
-                        tuple(product.category))
-            result = cur.fetchall()
-            for element in result:
-                s_products.append(
-                    cl.Product(element['name'], element['store'], element['nutrition_grade'], element['url'],
-                               element['category']))
-            return s_products
+    def get_substitutes(self, product_ng, category_id):
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cursor.execute("SELECT nutrition_grade, name FROM product INNER JOIN product_category ON "
+                       "product.id = product_category.product_id WHERE category_id = %s AND product.name "
+                       "IS NOT NULL AND product.nutrition_grade < %s ORDER BY product.nutrition_grade",
+                       (category_id, product_ng))
+        substitute_result = cursor.fetchall()
+        cursor.close()
 
-    def substitutes_browser(self, product):
-        substitutes = self.get_substitutes(product)
-        page_min = 0
-        page_max = 10
-        while True:
-            if len(substitutes) - page_max < 10 <= page_max:
-                page_max += len(substitutes) - page_max
-                if page_max < 10:
-                    page_min = 0
-                else:
-                    page_min = page_max - 10
-            if page_min < 0:
-                page_min = 0
-                page_max = 10
+        page_size = 10
+        page = 0
+        substitutes = substitute_result[(page * page_size):(page * page_size) + page_size]
+        for index, value in enumerate(substitutes):
+            print(index, value)
 
-            print("\nListe des {} substitution pour le produit \"{}\" : \n".format(len(substitutes), product.name))
-            if len(substitutes) == 0:
-                print("\nIl n'y a pas de substitut pour votre produit.\nRetour à la fiche produit.\n")
-                break
-            else:
-                for i in range(page_min, page_max):
-                    print("{} - {}".format(i + 1, substitutes[i].name))
+        substitutes_choice = int(input("Please choose your substitute: "))
+        print("You have chosen : ", substitutes[substitutes_choice])
+        user_substitute_choice = (substitutes[substitutes_choice])
 
-            uinput = input("\nEntrez: Numéro - selectionner un produit | > - page suivante |"
-                           " < - page précedente | 0 - revenir au produit\n")
-
-            if uinput == '0':
-                break
-            if uinput.isdigit():
-                self.print_product(substitutes[int(uinput) - 1])
-                continue
-            if uinput == '>':
-                page_min += 10
-                page_max += 10
-            if uinput == '<' and page_min > 0:
-                page_min -= 10
-                page_max -= 10
+        return user_substitute_choice
